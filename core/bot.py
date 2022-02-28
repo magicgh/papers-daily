291b99173cccb35e4eb8776f39e6e1eda76babc2
magicgh <magicgher@hotmail.com>
2022-02-28 13:17:07 +0800

    🐛 Fix format

--- core/bot.py
+++ core/bot.py
@@ -61,7 +61,7 @@ def convert_to_message(filename: str):
                     if v is not None:
                         if v["repo_url"] is not None:
                             f.write(
-                              f'• <a href="{v["paper_url"]}"><em>{v["title"]}</em></a>   <a href="{v["repo_url"]}"><em>&lt;Code&gt;</em></a>\n'
+                              f'• <a href="{v["paper_url"]}"><em>{v["title"]}</em></a> <a href="{v["repo_url"]}"><em>&lt;Code&gt;</em></a>\n'
                             )
                         else:
                             f.write(f'• <a href="{v["paper_url"]}"><em>{v["title"]}</em></a>\n')
